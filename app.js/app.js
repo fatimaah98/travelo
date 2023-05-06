bc7e9873c16b69370710571184d15c5a07678d4c
fatimaah <nassarifa98@gmail.com>
2023-05-06 12:07:23 -0700

    travelo project

---
+++ app.js/app.js
@@ -0,0 +1,6 @@
+function showMenu() {
+    document.querySelector("#menu-items").style.display = "flex";
+}
+function hiddenMenu() {
+    document.querySelector("#menu-items").style.display = "none";
+}
